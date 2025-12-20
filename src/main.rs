522e4a760d2b40eae56374a2f5d014b6d15a38ce
dduuukk <chrisvsbender@protonmail.com>
2025-12-20 15:26:26 -0800

    feat: env logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 pub mod config;
 mod gcal;
 
+use config::EnvConfig;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let config = EnvConfig::new().await;
+    if let Err(e) = config {
+        log::error!("Config init error {e:?}")
+    } else {
+        log::error!("Config init without error");
+    }
 }
